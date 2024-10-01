31a492070219c3921bea150fcc57301556cfbbfb
Chair-Penetration <joeri.gerard@gmail.com>
2024-10-01 11:44:41 +0200

    Slot machine Commit 1

---
+++ Trying to do sum IDK/Assets/Gamble.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gamble : MonoBehaviour
+{
+    [SerializeField]private TextMeshProUGUI _slot1;
+    [SerializeField]private TextMeshProUGUI _slot2;
+    [SerializeField]private TextMeshProUGUI _slot3;
+
+    public TextMeshProUGUI Slot1 { get { return _slot1; } }
+    public TextMeshProUGUI Slot2 { get { return _slot2; } }
+    public TextMeshProUGUI Slot3 { get { return _slot3; } }
+
+    public void PullLever()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
